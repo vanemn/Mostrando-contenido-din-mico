8bd4ab7d8556d943ecbf1da47e5ef5e08607503b
Vane <vane.moralesn@gmail.com>
2024-10-14 19:27:04 -0300

    Pasar mediante contexto a la vista el listado de nombres con los empleados. utilizo context {'empleados': empleados}

--- site_institucion/app_empleados/views.py
+++ site_institucion/app_empleados/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def lista_empleados(request):
+    empleados = ['Vanessa Morales','Brian Diaz','Natalia Peña', 'Nicole Pinilla' , 'Valery Maragaño']
+    return render(request, 'app_empleados/lista_empleados.html',{'empleados': empleados})
